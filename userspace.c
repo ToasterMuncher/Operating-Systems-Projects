44c2cbb9c773da3c9c46745bef993877dc50b19e
Julio Anacleto <73974060+ToasterMuncher@users.noreply.github.com>
2025-10-11 14:56:27 -0700

    Delete userspace.c

--- userspace.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <linux/kernel.h>
-#include <sys/syscall.h>
-#include <unistd.h>
-int main() {
-	long int amma = syscall(463);
-	printf("System call sys_hello returned %ld\n", amma);
-	return 0;
-}
