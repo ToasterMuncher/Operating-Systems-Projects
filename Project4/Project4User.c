489827c76769f51b77393b20bec03406db537a84
Abdullah Almaroof <abdullahaalmaroof@gmail.com>
2025-12-12 19:39:59 -0800

    Implement user input for LED blink count
    Added user input for LED blinking times and validation.

--- Project4/Project4User.c
+++ Project4/Project4User.c
@@ -9,25 +9,47 @@
 #include <stdio.h>
 int main(int argc, char* argv[]) {
 	int fd = open("/dev/etx_device", O_RDWR);
+	if(fd < 0) {
+    printf("Error: Cannot open device file. Is the driver loaded?\n");
+    return 1;
+	}
 
-	// Write to turn LED on
-	char cmd = '1';
+	char cmd = '0';
 	write(fd, &cmd, 1);
 
-	// Read LED state
-	char state[3];
-	int n = read(fd, state, 2);
-	state[n] = '\0';
-	printf("LED is: %s", state);
+	int UserInput;
 
-	// Write to turn LED off
-	cmd = '0';
-	write(fd, &cmd, 1);
+	printf("Please enter how many times to blink (Positive Integer Values ONLY):");
+	scanf("%d", &UserInput);
 
-	// Read LED state
-	n = read(fd, state, 2);
-	state[n] = '\0';
-	printf("LED is: %s", state);
+	if(UserInput <=0){
+		printf("Error: Value Input is Invalid\n");
+		close(fd);
+		return 1;
+	}
+
+	while(UserInput > 0){
+		// Write to turn LED on
+		cmd = '1';
+		write(fd, &cmd, 1);
+		printf("LED IS ON\n");
+
+		sleep(2);
+
+		// Write to turn LED off
+		cmd = '0';
+		write(fd, &cmd, 1);
+		printf("LED IS OFF\n");
+
+		sleep (2);
+
+		UserInput--;
+
+	}
+
+	printf("Program Completed\n");
 
 	close(fd);
+
+	return 0;
 }
